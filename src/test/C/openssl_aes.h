9d6f05270bedad58a724f30a27e047dfbed2f5ed
Daniel Fan <danielfan@bwrcrdsl-1.eecs.berkeley.edu>
2021-02-23 10:29:30 -0800

    move C tests to separate repo

--- src/test/C/openssl_aes.h
+++
@@ -1,5 +0,0 @@
-#include "openssl/evp.h"
-
-int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, EVP_CIPHER_CTX *e_ctx, EVP_CIPHER_CTX *d_ctx);
-unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len);
-unsigned char *aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len);
