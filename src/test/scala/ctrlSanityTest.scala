44cfb78fe4fbfa720396d307bab09c75b6013337
TsaiAnson <ansonytsai@gmail.com>
2021-02-27 12:36:59 -0800

    Added verif dependency

--- src/test/scala/ctrlSanityTest.scala
+++ src/test/scala/ctrlSanityTest.scala
@@ -4,15 +4,10 @@ import org.scalatest._
 import chisel3._
 import chisel3.experimental.BundleLiterals._
 import chiseltest._
-import chiseltest.experimental.TestOptionBuilder._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import freechips.rocketchip.subsystem.WithoutTLMonitors
 import freechips.rocketchip.config.Parameters
-import chiseltest.internal.WriteVcdAnnotation
-import freechips.rocketchip.tile.OpcodeSet
-import freechips.rocketchip.diplomacy.LazyModule
-import chiseltest.internal.VerilatorBackendAnnotation
+import verif._
+
 
 
 class ctrlSanityTest extends AnyFlatSpec with ChiselScalatestTester {
--- src/test/scala/dcplrSanityTest.scala
+++ src/test/scala/dcplrSanityTest.scala
@@ -4,15 +4,9 @@ import org.scalatest._
 import chisel3._
 import chisel3.experimental.BundleLiterals._
 import chiseltest._
-import chiseltest.experimental.TestOptionBuilder._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import freechips.rocketchip.subsystem.WithoutTLMonitors
 import freechips.rocketchip.config.Parameters
-import chiseltest.internal.WriteVcdAnnotation
-import freechips.rocketchip.tile.OpcodeSet
-import freechips.rocketchip.diplomacy.LazyModule
-import chiseltest.internal.VerilatorBackendAnnotation
+import verif._
 
 class dcplrSanityTest extends AnyFlatSpec with ChiselScalatestTester {
   it should "elaborate the Decoupler" in {
