e175e91963049ea55d81feae521d4b9899ec8be6
TsaiAnson <ansonytsai@gmail.com>
2021-03-01 17:22:28 -0800

    Added VIP usage and test example/walkthrough for eric

--- src/test/scala/TestUtils.scala
+++ src/test/scala/TestUtils.scala
@@ -1,11 +1,14 @@
 package aes
 
 import chisel3._
+import chisel3.experimental.BundleLiterals._
 import chipsalliance.rocketchip.config.Parameters
+import freechips.rocketchip.rocket.HellaCacheResp
 import freechips.rocketchip.tile.RoCCCommand
 import verif.VerifBundleUtils.{RoCCCommandHelper, RoCCInstructionHelper}
 
 package object AESTestUtils {
+  // Helper methods for creating a RoCCCommand
   def keyLoad128(addr: Int)(implicit p: Parameters): RoCCCommand = RoCCCommandHelper(inst = RoCCInstructionHelper(xs1 = true.B), rs1 = addr.U)
   def keyLoad256(addr: Int)(implicit p: Parameters): RoCCCommand = RoCCCommandHelper(inst = RoCCInstructionHelper(funct = 1.U, xs1 = true.B), rs1 = addr.U)
   def addrLoad(src: Int, dest: Int)(implicit p: Parameters): RoCCCommand = RoCCCommandHelper(inst = RoCCInstructionHelper(funct = 2.U, xs1 = true.B, xs2 = true.B),
@@ -19,5 +22,12 @@ package object AESTestUtils {
   def getInterrupt(rd: Int)(implicit p: Parameters): RoCCCommand = {
     assert(rd < 32, s"RD register must be less than 32. Given: $rd")
     RoCCCommandHelper(inst = RoCCInstructionHelper(funct = 6.U, xd = true.B, rd = rd.U))
+  }
+
+  // Helper methods for creating a HellaCacheResp
+  // Note: I am unsure on what data_word, data_raw, and store_data is
+  def hellaCacheResp(data: Int)(implicit p: Parameters): HellaCacheResp = {
+    new HellaCacheResp().Lit(_.replay -> false.B, _.has_data -> true.B, _.data_word_bypass -> data.U(32.W),
+      _.data_raw -> data.U(32.W), _.store_data -> data.U(32.W))
   }
 }
--- src/test/scala/ctrlSanityTest.scala
+++ src/test/scala/ctrlSanityTest.scala
@@ -1,19 +1,23 @@
 package aes
 
+import aes.AESTestUtils.hellaCacheResp
 import org.scalatest._
 import chisel3._
 import chisel3.experimental.BundleLiterals._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.rocket.{HellaCacheReq, HellaCacheResp}
 import verif._
+
+import scala.util.Random
 
 
 
 class ctrlSanityTest extends AnyFlatSpec with ChiselScalatestTester {
-  it should "elaborate the Controller" in {
-    implicit val p: Parameters = VerifTestUtils.getVerifParameters()
+  implicit val p: Parameters = VerifTestUtils.getVerifParameters(xLen = 32) // 32-bit processor
 
+  it should "elaborate the Controller" in {
     //.withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation))
     test(new AESController()) { c =>
       assert(true)
@@ -21,7 +25,6 @@ class ctrlSanityTest extends AnyFlatSpec with ChiselScalatestTester {
   }
 
   it should "test controller state transitions" in {
-    implicit val p: Parameters = VerifTestUtils.getVerifParameters()
     val num_block = 2
     test(new AESController()) { c =>
       c.io.dmem.req.ready.poke(true.B) // memory always ready
@@ -64,6 +67,94 @@ class ctrlSanityTest extends AnyFlatSpec with ChiselScalatestTester {
       }
 
       println("------- ALL DONE -------")
+    }
+  }
+
+  // To Eric: Here is an example of a test using the drivers/monitors to mimic the HellaCache Interface
+  // It does not act like the true cache, but at least we can receive requests and send responses
+  it should "test key memory access" in {
+    test(new AESController()) { c =>
+      // Initializing RoCCCommand driver, receiver (dummy), and monitor
+      val driver = new verif.ValidDriver[HellaCacheResp](c.clock, c.io.dmem.resp) // Used to send responses, takes in ValidTX
+      val monitor = new DecoupledMonitor[HellaCacheReq](c.clock, c.io.dmem.req) // Used to observe all transactions on this interface
+      val receiver = new DecoupledDriverSlave[HellaCacheReq](c.clock, c.io.dmem.req, 0) // Acts as a dummy receiver (does nothing but hold ready high)
+
+      val r = new Random
+
+      // Initializing decoupler <> controller interface
+      c.io.dcplrIO.excp_valid.poke(false.B)
+      c.io.dcplrIO.key_valid.poke(false.B)
+      c.io.dcplrIO.key_size.poke(0.U(1.W))
+      c.io.dcplrIO.key_addr.poke(0.U(32.W))
+      c.io.dcplrIO.addr_valid.poke(false.B)
+      c.io.dcplrIO.src_addr.poke(0.U(32.W))
+      c.io.dcplrIO.dest_addr.poke(0.U(32.W))
+      c.io.dcplrIO.start_valid.poke(false.B)
+      c.io.dcplrIO.op_type.poke(false.B)
+      c.io.dcplrIO.block_count.poke(0.U(32.W))
+      c.clock.step()
+
+      // Reset (just to be safe)
+      c.io.reset.poke(true.B)
+      c.clock.step(r.nextInt(10))
+      c.io.reset.poke(false.B)
+      c.clock.step()
+
+      // Check state here
+      // assert(c.io.state == IDLE)...
+
+      // This is not necessary since the FSM should be in IDLE (from above assert), but it works as an example
+      while (!c.io.dcplrIO.key_ready.peek.litToBoolean) {
+        c.clock.step()
+      }
+
+      // Triggering key expansion
+      c.io.dcplrIO.key_valid.poke(true.B)
+      val op_type = r.nextInt(2)
+      c.io.dcplrIO.key_size.poke(op_type.U(1.W))
+      val addr = r.nextInt(2 << 32)
+      c.io.dcplrIO.key_addr.poke(addr.U(32.W))
+      c.clock.step()
+
+      while (c.io.dcplrIO.key_ready.peek.litToBoolean) {
+        c.clock.step()
+      }
+      c.io.dcplrIO.key_valid.poke(false.B)
+      c.clock.step()
+
+      // Check state here
+      // assert(c.io.state == KEY...)...
+
+      // Here we check to see if we got a response. I increment by a fixed number of cycles, but this can be changed
+      c.clock.step(10)
+
+      // 4 or 8 times depending on key size
+      val times = if (op_type == 1) 8 else 4
+      for (_ <- 0 until times) {
+        // Check that we actually received a memory request
+        assert(!monitor.monitoredTransactions.isEmpty)
+        val req = monitor.monitoredTransactions.head
+        monitor.clearMonitoredTransactions()
+        assert(req.data.addr.litValue() == addr)
+        // You should do more checks here
+
+        // Send a response
+        val resp_data = r.nextInt(2 << 32)
+        val resp = hellaCacheResp(resp_data)
+        driver.push(ValidTX(resp))
+        c.clock.step(r.nextInt(10) + 10) // This will vary, but for now, between 10-20 cycles
+
+        // Now you check if the data is correctly presented to the AES Core
+        // TODO: To Eric, fill this out as an exercise
+      }
+
+      c.clock.step(r.nextInt(100)) // Random, you can tweak this
+
+      // Check that there are no extra requests
+      assert(monitor.monitoredTransactions.isEmpty)
+
+      // To Eric: This is an example of a test with correct behavior. But what if something goes wrong? (e.g. data returns
+      // another data response? What is the correct FSM behavior?) You should write a few other tests. I will help you along the way.
     }
   }
 }
