218493336efb9549f7fb18a002e9ff870aec674a
TsaiAnson <ansonytsai@gmail.com>
2021-03-21 13:35:17 -0700

    Updated DMAInputBuffer IO: separated address and data, DMAInputBuffer now writes entire 128b blocks (previously 32b)

--- src/main/scala/DMABuffer.scala
+++ src/main/scala/DMABuffer.scala
@@ -7,21 +7,34 @@ import ee290cdma.EE290CDMAWriterReq
 // Takes 32bit inputs (from AES core) and writes to DMA
 class DMAInputBuffer (addrBits: Int = 32, beatBytes: Int) extends Module {
   val io = IO(new Bundle {
-    val dataIn  = Flipped(Decoupled(UInt((addrBits + 32).W)))
+    val baseAddr = Flipped(Decoupled(UInt(addrBits.W)))
+    val dataIn  = Flipped(Decoupled(UInt(32.W)))
     val dmaOutput = Decoupled(new EE290CDMAWriterReq(addrBits, beatBytes))
   })
 
-  val bitsFilled = RegInit(0.U(log2Ceil(32 + 1).W))
+  val bitsFilled = RegInit(0.U(log2Ceil(128 + 1).W))
   val addrReg = RegInit(0.U(addrBits.W))
-  val wideData = RegInit(0.U(32.W)) // Max data we will ever write at a time is 32b
-  // Can fill up beatBytes, but not necessary
-  val dataQueue = Module(new Queue(UInt((addrBits + 32).W), 4))
+  val wideData = RegInit(0.U(128.W)) // AES data block size is 128b
+  // AES core outputs 32 bits at a time
+  val dataQueue = Module(new Queue(UInt(32.W), 4))
+  // Signal when to start writing to DMA (ensure that all 128b of data is correctly matched to address)
+  val startWrite = RegInit(false.B)
 
-  // NOTE: These two statements will NEVER concurrently fire (fire conditions prevent)
+  // Start writing when we have an entire block of data (128b)
+  when (bitsFilled === 128.U) {
+    startWrite := true.B
+  } .elsewhen (bitsFilled === 0.U) {
+    startWrite := false.B
+  }
+
+  // NOTE: Base addr will only be set once per block in memory controller FSM
+  when (io.baseAddr.fire()) {
+    addrReg := io.baseAddr.bits
+  }
+  // NOTE: The two statements below will NEVER concurrently fire (fire conditions prevent)
   when (dataQueue.io.deq.fire()) {
-    addrReg := dataQueue.io.deq.bits(32 + addrBits - 1, 32)
-    wideData := dataQueue.io.deq.bits(31, 0)
-    bitsFilled := 32.U
+    wideData := wideData | (dataQueue.io.deq.bits << bitsFilled).asUInt()
+    bitsFilled := bitsFilled + 32.U
   }
   when (io.dmaOutput.fire()) {
     addrReg := addrReg + io.dmaOutput.bits.totalBytes
@@ -29,15 +42,17 @@ class DMAInputBuffer (addrBits: Int = 32, beatBytes: Int) extends Module {
     bitsFilled := bitsFilled - (io.dmaOutput.bits.totalBytes * 8.U)
   }
 
-  dataQueue.io.deq.ready := bitsFilled === 0.U
+  io.baseAddr.ready := ~startWrite
+  dataQueue.io.deq.ready := ~startWrite
   dataQueue.io.enq <> io.dataIn
-  io.dmaOutput.valid := bitsFilled > 0.U
+  io.dmaOutput.valid := (bitsFilled > 0.U) & startWrite
   io.dmaOutput.bits.addr := addrReg
   io.dmaOutput.bits.data := wideData
-  when (beatBytes.U <  4.U) {
+  when (beatBytes.U < 16.U) {
     io.dmaOutput.bits.totalBytes := beatBytes.U
   } .otherwise {
-    io.dmaOutput.bits.totalBytes := 4.U
+    // 16 bytes == 128 bits
+    io.dmaOutput.bits.totalBytes := 16.U
   }
 }
 
--- src/test/scala/DMABufferTest.scala
+++ src/test/scala/DMABufferTest.scala
@@ -1,105 +1,179 @@
 package aes
 
 import chisel3._
-import chisel3.util._
+import chisel3.util.DecoupledIO
 import chiseltest._
 import chiseltest.experimental.TestOptionBuilder._
-import chiseltest.internal.{TreadleBackendAnnotation, WriteVcdAnnotation}
+import chiseltest.internal.WriteVcdAnnotation
 import ee290cdma.EE290CDMAWriterReq
 import org.scalatest.flatspec.AnyFlatSpec
 import verif._
 
 class DMABufferTest extends AnyFlatSpec with ChiselScalatestTester {
-  it should "Test DMAInputBuffer when beatBytes == 4" in {
-    val beatBytes = 4
+
+  // ---------------------------- DMA Input Buffer Tests ----------------------------
+  // Testing various beatByte sizes centered at 16 (128bits) for complete block write
+
+  it should "Test DMAInputBuffer when beatBytes == 16" in {
+    val beatBytes = 16
     test(new DMAInputBuffer(32, beatBytes)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      val addrDriver = new DecoupledDriverMaster(c.clock, c.io.baseAddr)
       val inDriver = new DecoupledDriverMaster(c.clock, c.io.dataIn)
       val outDriver = new DecoupledDriverSlave[EE290CDMAWriterReq](c.clock, c.io.dmaOutput, 0)
       val outMonitor = new DecoupledMonitor[EE290CDMAWriterReq](c.clock, c.io.dmaOutput)
 
-      val addrInputs = Seq.fill(100)(BigInt(32, scala.util.Random))
-      val dataInputs = Seq.fill(100)(BigInt((beatBytes * 8), scala.util.Random))
-      val combInputs = addrInputs.zip(dataInputs).map(x => (x._1 << 32) + x._2)
+      val addrInputs = Seq.fill(25)(BigInt(32, scala.util.Random))
+      val resultDataBlocks = Seq.fill(25)(BigInt(128, scala.util.Random))
+      val mask = BigInt("1" * 32, 2) // For masking into 32bit blocks
 
-      inDriver.push(combInputs.map(x => new DecoupledTX(UInt((32 + (beatBytes * 8)).W)).tx(x.U)))
-      c.clock.step(combInputs.length + 200)
+      // Driving inputs
+      for (i <- addrInputs.indices) {
+        addrDriver.push(new DecoupledTX(UInt(32.W)).tx(addrInputs(i).U))
+        val dataBlock = resultDataBlocks(i)
+        val dataInputs = Seq(dataBlock & mask, (dataBlock >> 32) & mask, (dataBlock >> 64) & mask, dataBlock >> 96)
+        inDriver.push(dataInputs.map(x => new DecoupledTX(UInt(32.W)).tx(x.U)))
+        c.clock.step(15)
+      }
 
       assert(outMonitor.monitoredTransactions.nonEmpty)
-      assert(outMonitor.monitoredTransactions.size == combInputs.size)
+      assert(outMonitor.monitoredTransactions.size == resultDataBlocks.size)
       outMonitor.monitoredTransactions
         .map(x => x.data.addr.litValue())
         .zip(addrInputs)
         .foreach {case (o, e) => assert(o == e)}
       outMonitor.monitoredTransactions
         .map(x => x.data.data.litValue())
-        .zip(dataInputs)
+        .zip(resultDataBlocks)
         .foreach {case (o, e) => assert(o == e)}
       outMonitor.monitoredTransactions
         .map(x => assert(x.data.totalBytes.litValue() == beatBytes))
     }
   }
 
-  it should "Test DMAInputBuffer when beatBytes == 2 (less than 4)" in {
-    val beatBytes = 2
-    test(new DMAInputBuffer(32, beatBytes)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-      val inDriver = new DecoupledDriverMaster(c.clock, c.io.dataIn)
-      val outDriver = new DecoupledDriverSlave[EE290CDMAWriterReq](c.clock, c.io.dmaOutput, 0)
-      val outMonitor = new DecoupledMonitor[EE290CDMAWriterReq](c.clock, c.io.dmaOutput)
-
-      val addrInputs = Seq.fill(100)(BigInt(32, scala.util.Random))
-      val dataInputs = Seq.fill(100)(BigInt(32, scala.util.Random))
-      val combInputs = addrInputs.zip(dataInputs).map(x => (x._1 << 32) + x._2)
-
-      var addrResults = Seq[BigInt]()
-      for (i <- addrInputs) {
-        addrResults = addrResults ++ Seq(i, i + 2)
-      }
-      var dataResults = Seq[BigInt]()
-      for (i <- dataInputs) {
-        dataResults = dataResults ++ Seq(i & BigInt("1" * (beatBytes * 8), 2), i >> (beatBytes * 8))
-      }
-      println(s"Size of results: addrResults ${addrResults.size}, dataResults ${dataResults.size}")
-
-      inDriver.push(combInputs.map(x => new DecoupledTX(UInt((32 + (beatBytes * 8)).W)).tx(x.U)))
-      c.clock.step(combInputs.length*2 + 200)
-
-      assert(outMonitor.monitoredTransactions.nonEmpty)
-
-      assert(outMonitor.monitoredTransactions.size == addrResults.size &&
-        addrResults.size == dataResults.size)
-      outMonitor.monitoredTransactions
-        .map(x => x.data.addr.litValue())
-        .zip(addrResults)
-        .foreach {case (o, e) => assert(o == e)}
-
-      outMonitor.monitoredTransactions
-        .map(x => x.data.data.litValue())
-        .zip(dataResults)
-        .foreach {case (o, e) => assert(o == e)}
-
-      outMonitor.monitoredTransactions
-        .map(x => assert(x.data.totalBytes.litValue() == beatBytes))
-    }
-  }
-
-  it should "Test DMAInputBuffer when beatBytes == 8 (greater than 4)" in {
+  it should "Test DMAInputBuffer when beatBytes == 8 (less than 16)" in {
     val beatBytes = 8
     test(new DMAInputBuffer(32, beatBytes)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      val addrDriver = new DecoupledDriverMaster(c.clock, c.io.baseAddr)
       val inDriver = new DecoupledDriverMaster(c.clock, c.io.dataIn)
       val outDriver = new DecoupledDriverSlave[EE290CDMAWriterReq](c.clock, c.io.dmaOutput, 0)
       val outMonitor = new DecoupledMonitor[EE290CDMAWriterReq](c.clock, c.io.dmaOutput)
 
-      val addrInputs = Seq.fill(10)(BigInt(32, scala.util.Random))
-      val dataInputs = Seq.fill(10)(BigInt(32, scala.util.Random))
-      val combInputs = addrInputs.zip(dataInputs).map(x => (x._1 << 32) + x._2)
+      val addrInputs = Seq.fill(25)(BigInt(32, scala.util.Random))
+      val dataBlocks = Seq.fill(25)(BigInt(128, scala.util.Random))
 
-      inDriver.push(combInputs.map(x => new DecoupledTX(UInt((32 + (beatBytes * 8)).W)).tx(x.U)))
-      c.clock.step(combInputs.length + 200)
+      // Expected results
+      var resultAddrInputs = Seq[BigInt]()
+      for (i <- addrInputs) {
+        resultAddrInputs = resultAddrInputs ++ Seq(i, i + 8)
+      }
+      var resultDataBlocks = Seq[BigInt]()
+      for (i <- dataBlocks) {
+        resultDataBlocks = resultDataBlocks ++ Seq(i & BigInt("1" * 64, 2), i >> 64)
+      }
+      val mask = BigInt("1" * 32, 2) // For masking into 32bit blocks
+
+      // Driving inputs
+      for (i <- addrInputs.indices) {
+        addrDriver.push(new DecoupledTX(UInt(32.W)).tx(addrInputs(i).U))
+        val dataBlock = dataBlocks(i)
+        val dataInputs = Seq(dataBlock & mask, (dataBlock >> 32) & mask, (dataBlock >> 64) & mask, dataBlock >> 96)
+        inDriver.push(dataInputs.map(x => new DecoupledTX(UInt(32.W)).tx(x.U)))
+        c.clock.step(15)
+      }
 
       assert(outMonitor.monitoredTransactions.nonEmpty)
 
+      assert(outMonitor.monitoredTransactions.size == resultAddrInputs.size &&
+        resultAddrInputs.size == resultDataBlocks.size)
+      outMonitor.monitoredTransactions
+        .map(x => x.data.addr.litValue())
+        .zip(resultAddrInputs)
+        .foreach {case (o, e) => assert(o == e)}
+
+      outMonitor.monitoredTransactions
+        .map(x => x.data.data.litValue())
+        .zip(resultDataBlocks)
+        .foreach {case (o, e) => assert(o == e)}
+
+      outMonitor.monitoredTransactions
+        .map(x => assert(x.data.totalBytes.litValue() == beatBytes))
+    }
+  }
+
+  // Testing one more case for beatBytes < 16, as this is most likely the param in the actual chip
+  it should "Test DMAInputBuffer when beatBytes == 4 (less than 16)" in {
+    val beatBytes = 4
+    test(new DMAInputBuffer(32, beatBytes)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      val addrDriver = new DecoupledDriverMaster(c.clock, c.io.baseAddr)
+      val inDriver = new DecoupledDriverMaster(c.clock, c.io.dataIn)
+      val outDriver = new DecoupledDriverSlave[EE290CDMAWriterReq](c.clock, c.io.dmaOutput, 0)
+      val outMonitor = new DecoupledMonitor[EE290CDMAWriterReq](c.clock, c.io.dmaOutput)
+
+      val addrInputs = Seq.fill(25)(BigInt(32, scala.util.Random))
+      val dataBlocks = Seq.fill(25)(BigInt(128, scala.util.Random))
+
+      // Expected results
+      var resultAddrInputs = Seq[BigInt]()
+      for (i <- addrInputs) {
+        resultAddrInputs = resultAddrInputs ++ Seq(i, i + 4, i + 8, i + 12)
+      }
+      val mask = BigInt("1" * 32, 2) // For masking into 32bit blocks
+      var resultDataBlocks = Seq[BigInt]()
+      for (i <- dataBlocks) {
+        resultDataBlocks = resultDataBlocks ++ Seq(i & mask, (i >> 32) & mask, (i >> 64) & mask, i >> 96)
+      }
+
+      // Driving inputs
+      for (i <- addrInputs.indices) {
+        addrDriver.push(new DecoupledTX(UInt(32.W)).tx(addrInputs(i).U))
+        val dataBlock = dataBlocks(i)
+        val dataInputs = Seq(dataBlock & mask, (dataBlock >> 32) & mask, (dataBlock >> 64) & mask, dataBlock >> 96)
+        inDriver.push(dataInputs.map(x => new DecoupledTX(UInt(32.W)).tx(x.U)))
+        c.clock.step(15)
+      }
+
+      assert(outMonitor.monitoredTransactions.nonEmpty)
+
+      assert(outMonitor.monitoredTransactions.size == resultAddrInputs.size &&
+        resultAddrInputs.size == resultDataBlocks.size)
+      outMonitor.monitoredTransactions
+        .map(x => x.data.addr.litValue())
+        .zip(resultAddrInputs)
+        .foreach {case (o, e) => assert(o == e)}
+
+      outMonitor.monitoredTransactions
+        .map(x => x.data.data.litValue())
+        .zip(resultDataBlocks)
+        .foreach {case (o, e) => assert(o == e)}
+
+      outMonitor.monitoredTransactions
+        .map(x => assert(x.data.totalBytes.litValue() == beatBytes))
+    }
+  }
+
+  it should "Test DMAInputBuffer when beatBytes == 32 (greater than 16)" in {
+    val beatBytes = 32
+    test(new DMAInputBuffer(32, beatBytes)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      val addrDriver = new DecoupledDriverMaster(c.clock, c.io.baseAddr)
+      val inDriver = new DecoupledDriverMaster(c.clock, c.io.dataIn)
+      val outDriver = new DecoupledDriverSlave[EE290CDMAWriterReq](c.clock, c.io.dmaOutput, 0)
+      val outMonitor = new DecoupledMonitor[EE290CDMAWriterReq](c.clock, c.io.dmaOutput)
+
+      val addrInputs = Seq.fill(25)(BigInt(32, scala.util.Random))
+      val resultDataBlocks = Seq.fill(25)(BigInt(128, scala.util.Random))
+      val mask = BigInt("1" * 32, 2) // For masking into 32bit blocks
+
+      for (i <- addrInputs.indices) {
+        addrDriver.push(new DecoupledTX(UInt(32.W)).tx(addrInputs(i).U))
+        val dataBlock = resultDataBlocks(i)
+        val dataInputs = Seq(dataBlock & mask, (dataBlock >> 32) & mask, (dataBlock >> 64) & mask, dataBlock >> 96)
+        inDriver.push(dataInputs.map(x => new DecoupledTX(UInt(32.W)).tx(x.U)))
+        c.clock.step(15)
+      }
+
+      assert(outMonitor.monitoredTransactions.nonEmpty)
       assert(outMonitor.monitoredTransactions.size == addrInputs.size &&
-        addrInputs.size == dataInputs.size)
+        addrInputs.size == resultDataBlocks.size)
       outMonitor.monitoredTransactions
         .map(x => x.data.addr.litValue())
         .zip(addrInputs)
@@ -107,13 +181,16 @@ class DMABufferTest extends AnyFlatSpec with ChiselScalatestTester {
 
       outMonitor.monitoredTransactions
         .map(x => x.data.data.litValue())
-        .zip(dataInputs)
+        .zip(resultDataBlocks)
         .foreach {case (o, e) => assert(o == e)}
 
       outMonitor.monitoredTransactions
-        .map(x => assert(x.data.totalBytes.litValue() == 4))
+        .map(x => assert(x.data.totalBytes.litValue() == 16)) // Max out at 16 bytes (128b)
     }
   }
+
+  // ---------------------------- DMA Output Buffer Tests ----------------------------
+  // Testing various beatByte sizes centered at 4 (32bits) or intended DMA output size
 
   it should "Test DMAOutputBuffer when beatBytes == 4" in {
     val beatBytes = 4
